ecf000fc1d11f9fa66b9d9b23fdbdfbd9b82088a
Cliff Gallagher <cgallagher@cvent.com>
2022-12-06 09:40:49 -0600

    return values for personName and taskDescription fields in PersonSchedule GET

--- src/main/java/com/cliff2/resources/PersonScheduleResource.java
+++ src/main/java/com/cliff2/resources/PersonScheduleResource.java
@@ -20,12 +20,33 @@ public class PersonScheduleResource {
 
     @GET
     @Produces("application/json")
-    public Response getTask() {
+    public Response getPersonSchedule() {
 
         List<PersonSchedule> personSchedules = jdbi.withHandle(handle -> {
             return handle.createQuery("SELECT * FROM \"person_schedules\" ORDER BY \"start_time\"")
                     .mapToBean(PersonSchedule.class)
                     .list();
+        });
+
+        personSchedules.forEach(personSchedule -> {
+
+            //get personName of each personSchedule
+            int personId = personSchedule.getPersonId();
+            String personName = jdbi.withHandle(handle -> {
+                return handle.select("SELECT name FROM person WHERE external_id = ?", personId)
+                        .mapTo(String.class)
+                        .one();
+            });
+            personSchedule.setPersonName(personName);
+
+            //get taskDescription of each personSchedule
+            int taskId = personSchedule.getTaskId();
+            String taskDescription = jdbi.withHandle(handle -> {
+                return handle.select("SELECT description FROM tasks WHERE external_id = ?", taskId)
+                        .mapTo(String.class)
+                        .one();
+            });
+            personSchedule.setTaskDescription(taskDescription);
         });
 
         return Response.status(Response.Status.OK).entity(personSchedules).build();
