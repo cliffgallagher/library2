98c047f82f4f99f0de083cb360655f3f541fdfd5
Cliff Gallagher <cgallagher@cvent.com>
2022-12-06 13:49:48 -0600

    task/unassigned displays only unassigned tasks

--- src/main/java/com/cliff2/resources/TaskResource.java
+++ src/main/java/com/cliff2/resources/TaskResource.java
@@ -1,5 +1,6 @@
 package com.cliff2.resources;
 
+import com.cliff2.api.PersonSchedule;
 import com.cliff2.api.Task;
 import org.jdbi.v3.core.Jdbi;
 
@@ -8,6 +9,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.List;
 
 @Path("/task")
@@ -20,13 +22,46 @@ public class TaskResource {
 
     @GET
     @Produces("application/json")
-    public Response getTask() {
+    public Response getAllTasks() {
 
         List<Task> tasks = jdbi.withHandle(handle -> {
             return handle.createQuery("SELECT * FROM \"tasks\" ORDER BY \"description\"")
                     .mapToBean(Task.class)
                     .list();
         });
+
+        return Response.status(Response.Status.OK).entity(tasks).build();
+
+    }
+
+    @GET
+    @Path("/unassigned")
+    @Produces("application/json")
+    public Response getUnassignedTasks() {
+
+        List<Task> tasks = jdbi.withHandle(handle -> {
+            return handle.createQuery("SELECT * FROM \"tasks\" ORDER BY \"description\"")
+                    .mapToBean(Task.class)
+                    .list();
+        });
+
+        List<PersonSchedule> personSchedules = jdbi.withHandle(handle -> {
+            return handle.createQuery("SELECT * FROM \"person_schedules\" ORDER BY \"start_time\"")
+                    .mapToBean(PersonSchedule.class)
+                    .list();
+        });
+
+        List<Task> toRemove = new ArrayList<>();
+        for (Task task: tasks) {
+            for (PersonSchedule personSchedule: personSchedules) {
+                if (task.getExternalId() == personSchedule.getTaskId()) {
+                    toRemove.add(task);
+                    break;
+                }
+            }
+        }
+
+        tasks.removeAll(toRemove);
 
         return Response.status(Response.Status.OK).entity(tasks).build();
 
